f318cba61b9ba38e504a0ffcedfa10e9a27e483e
Ahmed Mohamed <ahmed_noreldin@outlook.com>
2024-01-02 17:59:48 +0200

    test browser preview

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,34 @@
 
 export default function Home() {
   const testPrinter = async () => {
-    const res = await fetch(
-      "https://test-pos-printer-backend.vercel.app/api/print",
-      {
-        method: "POST",
-      }
-    );
-    console.log(res);
+    window.print();
   };
 
-  return <button onClick={async () => await testPrinter()}>Print Test</button>;
+  return (
+    <div>
+      <div id="printableArea" style={{ width: "80mm" }}>
+        <h1
+          style={{
+            textAlign: "center",
+            fontSize: "1.5em",
+            fontWeight: "bold",
+            margin: "0",
+          }}
+        >
+          Print me
+        </h1>
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: "1.2em",
+            fontWeight: "bold",
+            margin: "0",
+          }}
+        >
+          Welcome to the print test. This is a test to see if the printer works
+        </p>
+      </div>
+      <button onClick={async () => await testPrinter()}>Print Test</button>
+    </div>
+  );
 }
